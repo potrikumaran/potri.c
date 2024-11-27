a53b84ff559073a16c5f482c36b7c135ee9bca9e
potrikumaran <potrikumaran@gmail.com>
2024-11-27 16:33:24 +0530

    Add files via upload

---
+++ project1.cpp
@@ -0,0 +1,10 @@
+//project1
+#include<stdio.h>
+int main()
+{
+	int x,y,z;
+	scanf("%d%d%d",&x,&y,&z);
+	z=x+y;
+	printf("%d",z);
+	return 0;
+}
---
+++ swap.c
@@ -0,0 +1,10 @@
+ //swap//
+   #include<stdio.h>
+ int main()
+ {
+ 	int a,b,t;
+ 	scanf("%d%d",&a,&b);
+ 	t=a;a=b;b=t;
+ 	printf("%d%d",a,b);
+ 	return 0;
+ }
