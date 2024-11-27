1611b07e25389ea72752833857039cb8539e85ca
potrikumaran <potrikumaran@gmail.com>
2024-11-28 01:36:45 +0530

    Update w4new3.c

--- w4new3.c
+++ w4new3.c
@@ -1,15 +1,16 @@
 #include<stdio.h> 
 int main()
 {
-	int i,j,s,n;
+	int i,j,n;
 	scanf("%d",&n);
 	for(i=0;i<n;i++)
 	{
-		for(s=1;s<=n-i;s++){
-		
-		printf(" ");}
-		for(j=1;j<=i;j++)
-		printf("*");
+		for(j=1;j<=n;j++){
+			if((i+j)<=n)
+		printf(" ");
+		else
+		printf("*");}
 		printf("\n");
 	}
+	return 0;
 }
